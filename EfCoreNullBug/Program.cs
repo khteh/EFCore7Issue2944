6cae8ef79cc04246a7af423d9180b0107d59dbf1
Kok How, Teh <funcoolgeek@gmail.com>
2023-11-18 14:32:40 +0800

    Reproduce EF Core github issue #2944

--- EfCoreNullBug/Program.cs
+++ EfCoreNullBug/Program.cs
@@ -1,6 +1,6 @@
 ﻿using EfCoreNullBug;
 using Microsoft.Extensions.Configuration;
-
+using static System.Console;
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
@@ -9,26 +9,35 @@ var dbPort = configuration["DatabaseConnection:Port"];
 var dbDatabase = configuration["DatabaseConnection:Database"];
 var dbUserId = configuration["DatabaseConnection:User Id"];
 var dbPassword = configuration["DatabaseConnection:Password"];
-var connectionString = $"Server={dbServer};Port={dbPort};Database={dbDatabase};User Id={dbUserId};Password={dbPassword}";
+var connectionString = $"Server={dbServer};Port={dbPort};Database={dbDatabase};User Id={dbUserId};Password={dbPassword};Include Error Detail=true";
 
-using var db = new EfDbContext(connectionString);
-
-//create
-db.Add(new Person
+try
 {
-    Id = 1,
-    Name = "Me",
-});
-db.SaveChanges();
+    using var db = new EfDbContext(connectionString);
 
-//read
-var pers = db.Persons.SingleOrDefault(o => o.Id == 1);
+    //create
+    db.Add(new Person
+    {
+        Id = 1,
+        // XXX Ignoring this property string value will throw 23502: null value in column "Name" of relation "Persons" violates not-null constraint DETAIL: Failing row contains(1, null, , , , , null, 0, f).
+        Name = "Me",
+        // However, value object "Address" without initialized doesn't throw. Why is there such different behaviour?
+    });
+    db.SaveChanges();
 
-//update
-pers.Name = "You";
-pers.Age = 1000;
-db.SaveChanges();
+    //read
+    var pers = db.Persons.SingleOrDefault(o => o.Id == 1);
 
-//delete
-db.Remove(pers);
-db.SaveChanges();
+    //update
+    pers.Name = "You";
+    pers.Age = 1000;
+    db.SaveChanges();
+
+    //delete
+    db.Remove(pers);
+    db.SaveChanges();
+}
+catch (Exception e)
+{
+    WriteLine($"Exception! {e}");
+}
